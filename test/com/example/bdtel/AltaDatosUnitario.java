07470e457b63136f95822a4373c6dc5d71613c6c
juan <juanguadix69@gmail.com>
2023-01-16 13:10:53 +0100

    test 5 subida

--- test/com/example/bdtel/AltaDatosUnitario.java
+++ test/com/example/bdtel/AltaDatosUnitario.java
@@ -1,27 +1,17 @@
-package com.example.bdtel.DAO;
+package com.example.bdtel;
 
+import com.example.bdtel.DAO.MovilesDAO;
 import com.example.bdtel.Modelo.Moviles;
 import org.junit.jupiter.api.Test;
-import org.testfx.framework.junit5.Start;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MovilesDAOTest {
+class AltaDatosUnitario {
 
     static MovilesDAO mDao = new MovilesDAO();
     ;
     static Moviles m = new Moviles(1, "Samsung-Juan", "Samsung", 0, 2, "android el ejido", "el ejido", 3, 4, 5, null);
     ;
-
-
-
-
-    @Test
-    void borrarDato() {
-
-        assertEquals(1, mDao.borrarMovilTest(m));
-
-    }
 
     @Test
     void insertarDato() {
